bc9be34ee6e8927aa42cf32eb9de74aa6fff48ef
Hans Christian Winther-Sørensen <91313412+hwinther-tietoevry@users.noreply.github.com>
2023-11-21 16:52:45 +0100

    Add jetbrains EAP version to support SDK 8

--- src/installer.ts
+++ src/installer.ts
@@ -4,7 +4,7 @@ export class Installer {
   //TODO check dotnet sdk in constructor
   async install(version: string): Promise<number> {
     // The 'update' command will install JetBrains.ReSharper.GlobalTools if it is not already installed.
-    let command = `dotnet tool update --global JetBrains.ReSharper.GlobalTools`
+    let command = `dotnet tool update --global JetBrains.ReSharper.GlobalTools --version 2023.3.0-eap08` # TODO: remove version once these changes are out of eap
 
     if (version !== '') {
       command += ` --version ${version}`
